7c8f26238909cd6a1597208e4fd268b03cc15adc
AndersBNielsen <forstuvning@gmail.com>
2025-08-17 23:17:29 +0200

    2716 / TMS2532 support
    *ROM pin count must now be specified when calling the python scripts - this will turn on the VCC drivers.
    *Support for TMS2516 and M2716

--- send_binary.py
+++ send_binary.py
@@ -2,10 +2,10 @@ import sys
 import time
 import serial
 
-def send_file(serial_port, baud_rate, file_path, block_size):
+def send_file(serial_port, baud_rate, file_path, block_size, rom_pincount  ):
     try:
         # Open serial port
-        ser = serial.Serial(serial_port, baud_rate, timeout = 0.001, inter_byte_timeout=0.001, dsrdtr=True)
+        ser = serial.Serial(serial_port, baud_rate, timeout=0.001, inter_byte_timeout=0.001, dsrdtr=True)
         # Disable the DTR signal to prevent reset
         ser.dtr = False
         time.sleep(2)
@@ -19,7 +19,7 @@ def send_file(serial_port, baud_rate, file_path, block_size):
             ser.write(b'\x02') # Burn ROM command
             ser.write(bytes([block_size]))
             ser.write(b'\x10') # Stop page.. Fix
-
+            ser.write(bytes([rom_pincount])) # ROM pin count
             # Read the file and send in blocks
             while True:
                 # Wait for the recipient's readiness signal (AA byte)
@@ -52,8 +52,8 @@ def send_file(serial_port, baud_rate, file_path, block_size):
 
 if __name__ == "__main__":
     # Check if correct number of command line arguments is provided
-    if len(sys.argv) != 5:
-        print("Usage: python send_binary.py <serial_port> <baud_rate> <file_path> <block_size>")
+    if len(sys.argv) != 6:
+        print("Usage: python send_binary.py <serial_port> <baud_rate> <file_path> <block_size> <rom_pincount>")
         sys.exit(1)
 
     # Extract command line arguments
@@ -61,6 +61,7 @@ if __name__ == "__main__":
     baud_rate = int(sys.argv[2])
     file_path = sys.argv[3]
     block_size = int(sys.argv[4])
+    rom_pincount = int(sys.argv[5])  # Convert string to int
 
     # Call the function to send the file
-    send_file(serial_port, baud_rate, file_path, block_size)
+    send_file(serial_port, baud_rate, file_path, block_size, rom_pincount)
